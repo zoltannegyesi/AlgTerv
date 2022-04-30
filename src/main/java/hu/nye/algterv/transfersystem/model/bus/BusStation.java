385d2eca5a8bbc5b4f40f0608e9f426731d0f9c3
Négyesi Zoltán <zoltannegyesi4@gmail.com>
2022-04-30 21:57:00 +0200

    BusLine

--- src/main/java/hu/nye/algterv/transfersystem/model/bus/BusStation.java
+++ src/main/java/hu/nye/algterv/transfersystem/model/bus/BusStation.java
@@ -1,9 +1,14 @@
 package hu.nye.algterv.transfersystem.model.bus;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -22,8 +27,13 @@ public class BusStation {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer busStationId;
+
     private String busStationName;
-    private Long settlementId;
+
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private List<BusLine> settlementId;
+
     private String gps1;
+
     private String gps2;
 }
