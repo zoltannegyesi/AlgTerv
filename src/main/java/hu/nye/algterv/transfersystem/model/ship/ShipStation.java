8ffcf80074328de5ebe4d60214405b81af4d06b7
Négyesi Zoltán <zoltannegyesi4@gmail.com>
2022-04-30 22:34:13 +0200

    ShipStation

---
+++ src/main/java/hu/nye/algterv/transfersystem/model/ship/ShipStation.java
@@ -0,0 +1,42 @@
+package hu.nye.algterv.transfersystem.model.ship;
+
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import hu.nye.algterv.transfersystem.model.region.Settlement;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "airports")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ShipStation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer shipStationId;
+
+    private String shipStationName;
+
+    @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(name = "settlement_id", referencedColumnName = "settlementId")
+    private Settlement settlementId;
+
+    private String gps1;
+
+    private String gps2;
+}
+
