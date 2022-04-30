b9555830d7b3174acc0b2c8dc983e061f18c4020
Négyesi Zoltán <zoltannegyesi4@gmail.com>
2022-04-30 15:39:50 +0200

    Created project

---
+++ src/main/java/hu/nye/algterv/transfersystem/TransfersystemApplication.java
@@ -0,0 +1,13 @@
+package hu.nye.algterv.transfersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransfersystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransfersystemApplication.class, args);
+	}
+
+}
