c1e08d4f5d98ad54a2e058e8b7197f777aca7886
Négyesi Zoltán <zoltannegyesi4@gmail.com>
2022-06-09 08:49:24 +0200

    Sorting works

--- src/main/java/hu/nye/algterv/transfersystem/service/CountryService.java
+++ src/main/java/hu/nye/algterv/transfersystem/service/CountryService.java
@@ -7,6 +7,7 @@ import java.util.NoSuchElementException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.QAbstractAuditable;
 import org.springframework.stereotype.Service;
 
 import hu.nye.algterv.transfersystem.model.TravelInfo;
@@ -41,11 +42,6 @@ public class CountryService {
 
    private List<List<TravelInfo>> allTransports = new ArrayList<>(); 
 
-   private List<Flight> flights = new ArrayList<>();
-   private List<BusLine> busLines = new ArrayList<>();
-   private List<TrainLine> trainLines = new ArrayList<>();
-   private List<ShipLine> shipLines = new ArrayList<>();
-
    private List<TravelInfo> loadedTravelInfos = new ArrayList<>();
    
    @Autowired
@@ -55,12 +51,15 @@ public class CountryService {
        this.trainLineRepository = trainLineRepository;
        this.shipLineRepository = shipLineRepository;
        this.idFinder = idFinder;
-       flightRepository.findAll().forEach(f->loadedTravelInfos.add(new TravelInfo(f)));
-       busLineRepository.findAll().forEach(b->loadedTravelInfos.add(new TravelInfo(b)));
-       trainLineRepository.findAll().forEach(t->loadedTravelInfos.add(new TravelInfo(t)));
-       shipLineRepository.findAll().forEach(s->loadedTravelInfos.add(new TravelInfo(s)));
-
    }   
+
+   private void loadTravelInfos() {
+    loadedTravelInfos = new ArrayList<>();
+    flightRepository.findAll().forEach(f->loadedTravelInfos.add(new TravelInfo(f)));
+    busLineRepository.findAll().forEach(b->loadedTravelInfos.add(new TravelInfo(b)));
+    trainLineRepository.findAll().forEach(t->loadedTravelInfos.add(new TravelInfo(t)));
+    shipLineRepository.findAll().forEach(s->loadedTravelInfos.add(new TravelInfo(s)));
+   }
 
    public Settlement getStartId(String settlementName, SearchOptions searchOptions) {
        return this.idFinder.getStartSettlement(settlementName, searchOptions);
@@ -71,28 +70,94 @@ public class CountryService {
     }
 
     public void getRoute(Settlement from, Settlement to) {
+        loadTravelInfos();
         List<TravelInfo> startTravelInfos = getStartTravelInfos(from);
-        startTravelInfos.forEach(t->checkNext(t, to));
-        allTransports.forEach(a->{a.forEach(b->{System.out.println(b.getFromSettlement().getSettlementName() + " " + b.getToSettlement().getSettlementName() + " " + b.getTransportType());});System.out.println();});
-    
+        startTravelInfos.forEach(t->checkNext(t, to, from));
+        List<TravelInfo> allResults = getAllTravelInfos();
+        allTransports = new ArrayList<>();
+        asd(from, to, allResults);
+        System.out.println("\n\nasd");
+    allTransports.forEach(a->{a.forEach(b->{System.out.println(b.getFromSettlement().getSettlementName() + " " + b.getToSettlement().getSettlementName() + " " + b.getTransportType());});System.out.println();});
+
+    }
+
+    private List<List<TravelInfo>> asd(Settlement from, Settlement to, List<TravelInfo> allResults) {
+        System.out.println("start");
+        List<TravelInfo> deletableInfos = new ArrayList<>();
+        allResults.forEach(t->{
+            if (t.getFromSettlement().getSettlementId().equals(from.getSettlementId()) && t.getToSettlement().getSettlementId().equals(to.getSettlementId())) {
+                List<TravelInfo> temp = new ArrayList<>();
+                temp.add(t);
+                allTransports.add(temp);
+                deletableInfos.add(t);
+            }
+        });
+        deletableInfos.forEach(i->allResults.remove(i));
+        List<TravelInfo> firsts = allResults.stream().filter(t->t.getFromSettlement().getSettlementId().equals(from.getSettlementId())).toList();
+        List<List<TravelInfo>> otherInfos = new ArrayList<>();
+        deletableInfos.clear();
+        firsts.forEach(f->{
+            List<TravelInfo> temp = new ArrayList<>();
+            for (TravelInfo a: allResults) {
+                if (a.getFromSettlement().getSettlementId().equals(
+                    f.getToSettlement().getSettlementId()) && !deletableInfos.contains(a)) {
+                    temp.add(a);
+                    deletableInfos.add(a);
+                }
+            }
+            otherInfos.add(temp);
+            temp = new ArrayList<>();
+        });
+        List<TravelInfo> sortedFirsts = new ArrayList<>();
+        for (TravelInfo i : firsts) {
+            if (!sortedFirsts.contains(i)) {
+                sortedFirsts.add(i);
+            }
+        }
+        sortedFirsts.forEach(b->System.out.println(b.getFromSettlement().getSettlementName() + " " + b.getToSettlement().getSettlementName() + " " + b.getTransportType()));
+        System.out.println("\n");
+        otherInfos.forEach(a->{a.forEach(b->{System.out.println(b.getFromSettlement().getSettlementName() + " " + b.getToSettlement().getSettlementName() + " " + b.getTransportType());});System.out.println();});
+        sortedFirsts.forEach(f->{
+            System.out.println(f.getFromSettlement().getSettlementName() + " " + f.getToSettlement().getSettlementName() + " " + f.getTransportType());
+            for (List<TravelInfo> i : otherInfos){
+                if (!i.isEmpty()) {
+                    if (i.get(0).getFromSettlement().getSettlementId().equals(f.getToSettlement().getSettlementId())) {
+                        i.forEach(j->{
+                            List<TravelInfo> temp = new ArrayList<>();
+                            temp.add(f);
+                            temp.add(j);
+                            allTransports.add(temp);
+                        });
+                        
+                    }
+                }
+            };
+        });
+        System.out.println("finish");
+        return null;
+    }
+
+    private List<TravelInfo> getAllTravelInfos() {
+        List<TravelInfo> result = new ArrayList<>();
+        allTransports.forEach(t->t.forEach(result::add));
+        return result;
     }
 
     private List<TravelInfo> tempInfos = new ArrayList<>();
 
-
-    private void checkNext(TravelInfo travelInfo, Settlement to) {
+    private void checkNext(TravelInfo travelInfo, Settlement to, Settlement from) {
         tempInfos.add(travelInfo);
         if (travelInfo.getToSettlement().getSettlementId().equals(to.getSettlementId())) {
             LOGGER.info("Successfully found route");
             List<TravelInfo> temp = new ArrayList<>();
             tempInfos.forEach(temp::add);
             allTransports.add(temp);
-            tempInfos = new ArrayList<>();
+                tempInfos = new ArrayList<>();
             return;
         } else {
             List<TravelInfo> newTravelInfo = getStartTravelInfos(travelInfo.getToSettlement());
             if (!newTravelInfo.isEmpty()) {
-                newTravelInfo.forEach(f->checkNext(f, to));
+                newTravelInfo.forEach(f->checkNext(f, to, from));
             }            
         }  
     }
@@ -102,4 +167,5 @@ public class CountryService {
            return f.getFromSettlement().getSettlementId().equals(settlement.getSettlementId());
         }).toList();
     }
+
 }
